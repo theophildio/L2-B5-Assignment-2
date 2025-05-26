68319ee3af222871ad196888b5688524a78d7748
Theophil Dio <dev.theophildio@gmail.com>
2025-05-26 20:48:59 +0600

    Sample data inserted into species

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -46,3 +46,40 @@ VALUES (
         'Carol King',
         'Mountain Range'
     );
+
+INSERT INTO
+    species (
+        species_id,
+        common_name,
+        scientific_name,
+        discovery_date,
+        conservation_status
+    )
+VALUES (
+        1,
+        'Snow Leopard',
+        'Panthera uncia',
+        '1775-01-01',
+        'Endangered'
+    ),
+    (
+        2,
+        'Bengal Tiger',
+        'Panthera tigris tigris',
+        '1758-01-01',
+        'Endangered'
+    ),
+    (
+        3,
+        'Red Panda',
+        'Ailurus fulgens',
+        '1825-01-01',
+        'Vulnerable'
+    ),
+    (
+        4,
+        'Asiatic Elephant',
+        'Elephas maximus indicus',
+        '1758-01-01',
+        'Endangered'
+    );
