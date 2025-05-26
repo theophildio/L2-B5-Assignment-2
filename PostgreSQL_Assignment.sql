a65222f98976cfff1aba5efaff100b0d8805b330
Theophil Dio <dev.theophildio@gmail.com>
2025-05-26 20:37:14 +0600

    species table created

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -14,3 +14,11 @@ CREATE TABLE rangers (
     name VARCHAR(50) NOT NULL,
     region VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(50) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50) NOT NULL
+);
