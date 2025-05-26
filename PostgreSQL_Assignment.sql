70c18d51269c4c64fb43cef286f663dee5d117a3
Theophil Dio <dev.theophildio@gmail.com>
2025-05-26 20:32:27 +0600

    rangers table created

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,2 +1,16 @@
 -- Create the database
 CREATE DATABASE conservation_db;
+
+-- Drop tables if they already exists
+DROP TABLE IF EXISTS rangers;
+
+DROP TABLE IF EXISTS sightings;
+
+DROP TABLE IF EXISTS species;
+
+-- Create TABLES
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    region VARCHAR(100) NOT NULL
+);
