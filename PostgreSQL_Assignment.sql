ab36937a082d2687a698aedc0ad3b75c5e8ac313
Theophil Dio <dev.theophildio@gmail.com>
2025-05-26 21:10:15 +0600

    Sample data inserted into sightings

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -83,3 +83,45 @@ VALUES (
         '1758-01-01',
         'Endangered'
     );
+
+INSERT INTO
+    sightings (
+        sighting_id,
+        species_id,
+        ranger_id,
+        location,
+        sighting_time,
+        notes
+    )
+VALUES (
+        1,
+        1,
+        1,
+        'Peak Ridge',
+        '2024-05-10 07:45:00',
+        'Camera trap image captured'
+    ),
+    (
+        2,
+        2,
+        2,
+        'Bankwood Area',
+        '2024-05-12 16:20:00',
+        'Juvenile seen'
+    ),
+    (
+        3,
+        3,
+        3,
+        'Bamboo Grove East',
+        '2024-05-15 09:10:00',
+        'Feeding observed'
+    ),
+    (
+        4,
+        1,
+        2,
+        'Snowfall Pass',
+        '2024-05-18 18:30:00',
+        NULL
+    );
